7d4e6fc8c5965ff98590d89bd8d26ff71be58659
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-27 11:25:53 +0200

    Add basic e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,15 @@
-import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { v4 as uuidv4 } from 'uuid';
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication;
+  let access_token: string;
 
   beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
@@ -15,10 +17,82 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GroceryItemsModule', () => {
+    //create new user and get access token
+    beforeAll(async () => {
+      const user = {
+        email: `jest-${uuidv4()}@e2e.com`,
+        password: 'test',
+        name: 'e2e',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/register')
+        .send(user)
+        .expect(201);
+
+      const accessToken = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(user)
+        .expect(200);
+
+      access_token = accessToken.body.access_token;
+    });
+
+    // small script to remove all database entries for cat between tests
+    beforeEach(async () => {
+      // const uncleared = await request(app.getHttpServer()).get(
+      //   '/grocery-items',
+      // );
+      // await Promise.all(
+      //   uncleared.body.map(async (cat) => {
+      //     return request(app.getHttpServer()).delete(
+      //       `/grocery-items/${cat.id}`,
+      //     );
+      //   }),
+      // );
+      //TODO clear whole database after tests
+    });
+
+    it('Create new user, post grocery item, get all, get by id, delete', async () => {
+      const newGroceryItem = {
+        name: 'Test',
+        description: 'Russian Blue',
+        quantity: 1.5,
+      };
+      const data = await request(app.getHttpServer())
+        .post('/grocery-items')
+        .set('Authorization', 'Bearer ' + access_token)
+        .send(newGroceryItem)
+        .expect(201);
+      expect(data.body).toEqual({
+        ...newGroceryItem,
+        id: expect.any(String),
+      });
+      const groceryItems = await request(app.getHttpServer())
+        .get('/grocery-items')
+        .set('Authorization', 'Bearer ' + access_token)
+        .expect(200);
+      expect(groceryItems.body).toEqual(expect.any(Array));
+      expect(groceryItems.body.length).toBe(1);
+      expect(groceryItems.body[0]).toEqual({
+        ...newGroceryItem,
+        id: expect.any(String),
+      });
+      const sampleGroceryItem = await request(app.getHttpServer())
+        .get(`/grocery-items/${data.body.id}`)
+        .set('Authorization', 'Bearer ' + access_token)
+        .expect(200);
+      expect(sampleGroceryItem.body).toEqual(data.body);
+      return request(app.getHttpServer())
+        .delete(`/grocery-items/${data.body.id}`)
+        .set('Authorization', 'Bearer ' + access_token)
+        .expect(200)
+        .expect({ deleted: true });
+    });
   });
 });
