55ada9d5555d2206dee8000d9d47e03b123f0b87
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-15 16:18:09 +0200

    Add stub for UsersRepository test suite

---
+++ src/users/users.repository.spec.ts
@@ -0,0 +1,60 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { UserDocument } from './interfaces/user-document.interface';
+import { User } from './interfaces/user.interface';
+import { UsersRepository } from './users.repository';
+
+const mockUser: (
+  id?: string,
+  email?: string,
+  password?: string,
+  name?: string,
+) => User = (id, email, password, name) => {
+  return {
+    id,
+    email,
+    password,
+    name,
+  };
+};
+
+describe('UsersRepository', () => {
+  let repository: UsersRepository;
+  let model: Model<UserDocument>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersRepository,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockUser()),
+            constructor: jest.fn().mockResolvedValue(mockUser()),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            updateOne: jest.fn(),
+            findOneAndUpdate: jest.fn(),
+            create: jest.fn(),
+            remove: jest.fn(),
+            deleteOne: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    repository = module.get<UsersRepository>(UsersRepository);
+    model = module.get<Model<UserDocument>>(getModelToken('User'));
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
