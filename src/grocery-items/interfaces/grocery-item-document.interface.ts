8b39e0948062ed2cf88c6137fb4fca65c7ebf671
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-15 10:12:25 +0200

    Delete old redundant grocery item document interface

--- src/grocery-items/interfaces/grocery-item-document.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface GroceryItemDocument extends Document {
-  name: string;
-  quantity: number;
-  description?: string;
-  unit?: string;
-  userId?: string;
-}
