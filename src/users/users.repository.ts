3905048ce7199464ab1b03e984bb71b4dc3153d8
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-15 16:09:11 +0200

    Add UsersRepository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,7 +1,31 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { User } from './interfaces/user.interface';
+import { UserDocument } from './interfaces/user-document.interface';
 
 export interface IUsersRepository {
   findAll(): Promise<User[]>;
   findOneById(email: string): Promise<User>;
   createOne(user: User): Promise<User>;
+}
+
+@Injectable()
+export class UsersRepository implements IUsersRepository {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userModel.find({}).exec();
+  }
+
+  async findOneById(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }).exec();
+  }
+
+  async createOne(user: User): Promise<User> {
+    return await this.userModel.create(user);
+  }
 }
